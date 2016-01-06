9c1c99c2ae9539d375d1fddba0f2668101bd3e08
chetantarale <chetan@shippable.com>
2016-01-06 11:57:55 +0530

    updates path of composer

--- version/5_6.sh
+++ version/5_6.sh
@@ -1,22 +1,25 @@
 #!/bin/bash -e
 
 #Build PHP 5.6.7
-echo "============ Building PHP 5.6.7 =============="
-php-build -i development 5.6.7 $HOME/.phpenv/versions/5.6.7
+echo "============ Building PHP 5.6 =============="
+php-build -i development 5.6.7 $HOME/.phpenv/versions/5.6
 
 # Setting phpenv to 5.6.7
-echo "============ Setting phpenv to 5.6.7 ============"
+echo "============ Setting phpenv to 5.6 ============"
 phpenv rehash 
-phpenv global 5.6.7
+phpenv global 5.6
 
 # Install Composer
 echo "============ Installing Composer ============"
 curl -s http://getcomposer.org/installer | php
 chmod +x composer.phar
-mv composer.phar /usr/local/bin/composer
+mv composer.phar $HOME/.phpenv/versions/5.6/bin
 
 #install pickle
 cd /tmp/pickle
+echo '<---------------- Composer in 5.6 ---------------------------------->'
+which composer
+echo '<---------------- Composer in 5.6 ---------------------------------->'
 composer install
 
 # Install php extensions
