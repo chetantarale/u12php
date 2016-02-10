55b20ee268a26b98e49cf2ecaaff12420f9caf9d
harsh157 <harsh12213@gmail.com>
2016-02-10 22:35:15 +0530

    give path to phpenv in test

--- test/_php.sh
+++ test/_php.sh
@@ -2,7 +2,7 @@
 
 echo "============================ PHP Versions ==============================="
 echo "phpenv versions"
-phpenv versions
+$HOME/.phpenv/bin/phpenv versions
 printf "\n\n"
 
 declare -a versions=('5.4' '5.5' '5.6' '7.0')
@@ -11,7 +11,7 @@ for version in "${versions[@]}"
   do
     echo "=============== Switching to version $version  ======================"
     echo "phpenv local $version"
-    phpenv local $version
+    $HOME/.phpenv/bin/phpenv local $version
     printf "\n"
 
     echo "======================== Checking version = ========================="
