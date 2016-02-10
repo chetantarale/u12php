fdb7f33e9087998d0e16fcbcf6881d5d36e1c0f2
harsh157 <harsh12213@gmail.com>
2016-02-11 01:12:06 +0530

    correct php test

--- test/_php.sh
+++ test/_php.sh
@@ -9,11 +9,14 @@ declare -a versions=('5.4' '5.5' '5.6' '7.0')
 
 for version in "${versions[@]}"
   do
+    export PATH=$HOME/.phpenv/bin:$HOME/.phpenv/extensions:$PATH;
+    eval "$(phpenv init -)"
+ 
     echo "=============== Switching to version $version  ======================"
     echo "phpenv global $version"
     $HOME/.phpenv/bin/phpenv global $version
     printf "\n"
-
+    rm -rf ~/.composer && sudo ln -s ~/.phpenv/versions/$(phpenv version-name)/composer ~/.composer
     echo "======================== Checking version = ========================="
     echo "php --version"
     php --version
